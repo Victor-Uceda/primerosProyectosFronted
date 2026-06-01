41326789029eebe2d95e27547755d7e3746e16ec
uceda. <victorhugouceda@gmail.com>
2026-06-01 09:40:34 -0500

    Arreglo de conflictos GIT

--- SISTEMA-NOTAS/Alumno.js
+++ SISTEMA-NOTAS/Alumno.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Alumno {
     constructor(nombre, nota){
         this._nombre = nombre;
@@ -16,23 +15,4 @@ class Alumno {
     toString(){
         return `${this._nombre} - ${this._nota}`
     }
-=======
-class Alumno {
-    constructor(nombre, nota){
-        this._nombre = nombre;
-        this._nota = nota;
-    }
-
-    get nombre(){
-        return this._nombre;
-    }
-
-    get nota(){
-        return this._nota
-    }
-
-    toString(){
-        return `${this._nombre} - ${this._nota}`
-    }
->>>>>>> eeab32d949340c3755f33608a23c866ca781c733
 }
